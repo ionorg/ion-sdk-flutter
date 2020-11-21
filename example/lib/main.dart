fb412bad1722083d4f1f181c9a4f515d25eec639
cloudwebrtc <duanweiwei1982@gmail.com>
2020-11-21 14:27:09 +0800

    update.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:flutter_ion/flutter_ion.dart' as ion;
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(GetMaterialApp(
     home: Home(),
   ));
@@ -13,47 +14,49 @@ void main() {
 class Controller extends GetxController {
   Rx<RTCVideoRenderer> localRenderer = RTCVideoRenderer().obs;
   Rx<RTCVideoRenderer> remoteRenderer = RTCVideoRenderer().obs;
-  Future<void> setLocalSrcObject(MediaStream stream) async {
-    var old = localRenderer.value;
-    await old.initialize();
-    old.srcObject = stream;
-    localRenderer.value = old;
+
+  @override
+  @mustCallSuper
+  void onInit() {
+    super.onInit();
+    localRenderer.value.initialize();
+    remoteRenderer.value.initialize();
   }
 
-  Future<void> setRemoteSrcObject(MediaStream stream) async {
-    var old = remoteRenderer.value;
-    await old.initialize();
-    old.srcObject = stream;
-    remoteRenderer.value = old;
+  set localSrcObject(MediaStream stream) {
+    localRenderer.value.srcObject = stream;
+    localRenderer.refresh();
+  }
+
+  set remoteSrcObject(MediaStream stream) {
+    remoteRenderer.value.srcObject = stream;
+    remoteRenderer.refresh();
   }
 }
 
 class Home extends StatelessWidget {
   final Controller c = Get.put(Controller());
   final signalLocal = ion.JsonRPCSignal("ws://localhost:7000/ws");
-  ion.Client clientLocal;
-
-  Future<ion.LocalStream> connect() async {
-    if (clientLocal == null) {
-      clientLocal =
-          await ion.Client.create(sid: "test session", signal: signalLocal);
-      var localStream = await ion.LocalStream.getUserMedia(
-          constraints: ion.Constraints.defaults..simulcast = true);
-      clientLocal.publish(localStream);
-      return localStream;
-    }
-    return null;
-  }
+  ion.Client _client;
+  ion.LocalStream _localStream;
 
   void _echotest() async {
-    var localStream = await connect();
-    await c.setLocalSrcObject(localStream.stream);
-    clientLocal.ontrack = (track, ion.RemoteStream stream) {
-      print('ontrack: stream => ${stream.id}');
-      if (track.kind == 'video') {
-        c.setRemoteSrcObject(stream.stream);
-      }
-    };
+    if (_client == null) {
+      _client =
+          await ion.Client.create(sid: "test session", signal: signalLocal);
+
+      _localStream = await ion.LocalStream.getUserMedia(
+          constraints: ion.Constraints.defaults..simulcast = true);
+      _client.publish(_localStream);
+
+      c.localSrcObject = _localStream.stream;
+      _client.ontrack = (track, ion.RemoteStream stream) {
+        if (track.kind == 'video') {
+          print('ontrack: stream => ${stream.id}');
+          c.remoteSrcObject = stream.stream;
+        }
+      };
+    }
   }
 
   @override
