f9a4fa57f8280e7c751d9cd2aa5e31d15796163f
cloudwebrtc <duanweiwei1982@gmail.com>
2020-11-22 20:27:43 +0800

    Add pub/sub demo.

--- example/lib/views/pubsub_view.dart
+++ example/lib/views/pubsub_view.dart
@@ -6,16 +6,20 @@ import 'package:get/get.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:flutter_ion/flutter_ion.dart' as ion;
 
+class Participant {
+  Participant(this.title, this.renderer, this.stream);
+  MediaStream stream;
+  String title;
+  RTCVideoRenderer renderer;
+}
+
 class PubSubController extends GetxController {
-  Rx<RTCVideoRenderer> localRenderer = RTCVideoRenderer().obs;
-  Rx<RTCVideoRenderer> remoteRenderer = RTCVideoRenderer().obs;
+  List<Participant> plist = <Participant>[].obs;
 
   @override
   @mustCallSuper
   void onInit() {
     super.onInit();
-    localRenderer.value.initialize();
-    remoteRenderer.value.initialize();
   }
 
   ion.Signal _signal = ion.JsonRPCSignal("ws://192.168.1.2:7000/ws");
@@ -30,17 +34,20 @@ class PubSubController extends GetxController {
           constraints: ion.Constraints.defaults..simulcast = false);
       await _client.publish(_localStream);
 
-      _client.ontrack = (track, ion.RemoteStream stream) {
+      _client.ontrack = (track, ion.RemoteStream remoteStream) async {
         if (track.kind == 'video') {
-          print('ontrack: stream => ${stream.id}');
-          remoteSrcObject = stream.stream;
-          Timer(Duration(seconds: 4), () {
-            stream.preferLayer(ion.Layer.low);
-          });
+          print('ontrack: remote stream => ${remoteStream.id}');
+          var renderer = RTCVideoRenderer();
+          await renderer.initialize();
+          renderer.srcObject = remoteStream.stream;
+          plist.add(Participant('Remote', renderer, remoteStream.stream));
         }
       };
 
-      localSrcObject = _localStream.stream;
+      var renderer = RTCVideoRenderer();
+      await renderer.initialize();
+      renderer.srcObject = _localStream.stream;
+      plist.add(Participant('Local', renderer, _localStream.stream));
     } else {
       await _localStream.unpublish();
       _localStream.stream.getTracks().forEach((element) {
@@ -50,37 +57,59 @@ class PubSubController extends GetxController {
       _localStream = null;
       _client.close();
       _client = null;
-      localSrcObject = null;
     }
-  }
-
-  set localSrcObject(MediaStream stream) {
-    localRenderer.value.srcObject = stream;
-    localRenderer.refresh();
-  }
-
-  set remoteSrcObject(MediaStream stream) {
-    remoteRenderer.value.srcObject = stream;
-    remoteRenderer.refresh();
   }
 }
 
 class PubSubTestView extends StatelessWidget {
   final PubSubController c = Get.put(PubSubController());
 
+  Widget getItemView(Participant item) {
+    return Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                item.title,
+                style: TextStyle(fontSize: 18, color: Colors.black54),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                item.stream.id,
+                style: TextStyle(fontSize: 18, color: Colors.black54),
+              ),
+            ),
+            Expanded(
+              child: RTCVideoView(item.renderer,
+                  objectFit:
+                      RTCVideoViewObjectFit.RTCVideoViewObjectFitContain),
+            ),
+          ],
+        ));
+  }
+
   @override
-  Widget build(context) => Scaffold(
-      appBar: AppBar(title: Text("pub sub test")),
-      body: Center(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text("Local"),
-          Obx(() => Expanded(child: RTCVideoView(c.localRenderer.value))),
-          Text("Remote"),
-          Obx(() => Expanded(child: RTCVideoView(c.remoteRenderer.value)))
-        ],
-      )),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.video_call), onPressed: c.pubsub));
+  Widget build(context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("pub sub test")),
+        body: Container(
+            padding: EdgeInsets.all(10.0),
+            child: Obx(() => GridView.builder(
+                shrinkWrap: true,
+                itemCount: c.plist.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 5.0,
+                    crossAxisSpacing: 5.0,
+                    childAspectRatio: 1.0),
+                itemBuilder: (BuildContext context, int index) {
+                  return getItemView(c.plist[index]);
+                }))),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.video_call), onPressed: c.pubsub));
+  }
 }
